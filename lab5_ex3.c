b172a10677f1c0d588b95d2fd3e3b81b7783a242
Phuc Ha <50400585+phuclop2@users.noreply.github.com>
2020-09-23 11:56:20 +0700

    cc

--- lab5_ex3.c
+++ lab5_ex3.c
@@ -1,12 +1,15 @@
 //------------------------------------------- main.c CODE STARTS ---------------------------------------------------------------------------
+// if debounce, then it is good, if not the buzzer will ring 
 #include <stdio.h>
 #include "NUC100Series.h"
 #include "lab5_ex3.h"
 
 #define BUZZER_BEEP_TIME 3
-#define BUZZER_BEEP_DELAY 40000
-
+#define BUZZER_BEEP_DELAY 20000
 #define TIMER0_COUNTS 8000000
+
+void Buzzer_beep(int beep_time);
+void EINT1_IRQHandler(void);
 
 int main(void)
 {
@@ -39,7 +42,8 @@ TIMER0->TCSR |= (0x01ul << 26);
 
 //define Timer 0 operation mode
 TIMER0->TCSR &= ~(0x03ul << 27);
-//set one-shot mode
+TIMER0->TCSR |= (0x01ul << 27);
+//set periodic mode
     
 TIMER0->TCSR &= ~(0x01ul << 24);
 //CTB bit is set to 0, TDR is 24-bit up timer value 
@@ -56,9 +60,11 @@ TIMER0->TCMPR = TIMER0_COUNTS - 1;
 //LED display via GPIO-C12 to indicate main program execution
 PC->PMD &= (~(0x03ul << 24));
 PC->PMD |= (0x01ul << 24);
+
 //BUZZER to indicate interrupt handling routine
 PB->PMD &= (~(0x03ul << 22));
 PB->PMD |= (0x01ul << 22);
+
 //GPIO Interrupt configuration. GPIO-B15 is the interrupt source
 PB->PMD &= (~(0x03ul << 30));
 PB->IMD &= (~(1ul << 15));
@@ -66,8 +72,17 @@ PB->IEN |= (1ul << 15);
 //NVIC interrupt configuration for GPIO-B15 interrupt source
 NVIC->ISER[0] |= 1ul<<3;
 NVIC->IP[0] &= (~(3ul<<30));
+
 while(1){
-PC->DOUT ^= 1 << 12;
+
+PC->DOUT ^= 1ul << 12;
+    
+if(TIMER0->TCSR & (1ul << 25)){
+//stop counting
+TIMER0->TCSR &= ~(0x01ul << 30);
+Buzzer_beep(BUZZER_BEEP_TIME);
+}
+
 }
 }
 
@@ -77,17 +92,12 @@ void EINT1_IRQHandler(void){
 TIMER0->TCSR |= (0x01ul << 26);
 //start counting
 TIMER0->TCSR |= (0x01ul << 30);
-Buzzer_beep(BUZZER_BEEP_TIME);
-TIMER0->TCSR &= ~(0x01ul << 16);
-//TDR will not be updated continuously while timer counter is counting
-//stop counting
-TIMER0->TCSR &= ~(0x01ul << 30);
 PB->ISRC |= (1ul << 15);
 }
 
 void Buzzer_beep(int beep_time){
 int i;
-for(i=0;i<(beep_time*2);i++){
+for(i=0;i<(beep_time);i++){
 PB->DOUT ^= (1 << 11);
 CLK_SysTickDelay(BUZZER_BEEP_DELAY);
 }
